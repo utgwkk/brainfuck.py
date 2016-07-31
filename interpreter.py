0db77357ca626134d9bf0a7709c551a8676f9d11
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-08-01 03:35:09 +0900

    interpreter

---
+++ interpreter.py
@@ -0,0 +1,52 @@
+import sys
+
+def _generate_tree(program, ptr):
+    tree = []
+    while ptr < len(program):
+        if program[ptr] == '[':
+            leaf, nptr = _generate_tree(program, ptr + 1)
+            tree.append(leaf + [']'])
+            ptr = nptr
+        elif program[ptr] == ']':
+            return tree, ptr
+        else:
+            tree.append(program[ptr])
+        ptr += 1
+    return tree, 0
+
+
+def parse(program):
+    return _generate_tree(program, 0)[0]
+
+
+def _run(tree, memory, ptr):
+    for ch in tree:
+        if isinstance(ch, list):
+            ptr = _run(ch, memory, ptr)
+        elif ch == '+':
+            memory[ptr] += 1
+        elif ch == '-':
+            memory[ptr] -= 1
+        elif ch == '>':
+            ptr += 1
+            if ptr >= len(memory):
+                memory.append(0)
+        elif ch == '<':
+            ptr -= 1
+        elif ch == '.':
+            sys.stdout.write(chr(memory[ptr]))
+        elif ch == ',':
+            try:
+                memory[ptr] = ord(sys.stdin.read(1))
+            except TypeError:
+                memory[ptr] = 0
+        elif ch == ']':
+            if memory[ptr] != 0:
+                return _run(tree, memory, ptr)
+            else:
+                return ptr
+
+
+def run(tree):
+    return _run(tree, [0], 0)
+
